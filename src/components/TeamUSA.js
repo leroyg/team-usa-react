0575e8a5fd4a69bcacba9a201ca47e87e6d207da
leroyg <leroy.gardner@gmail.com>
2019-06-09 14:27:03 -0400

    added more athletes, checked some photo urls

--- src/components/TeamUSA.js
+++ src/components/TeamUSA.js
@@ -5,7 +5,7 @@ import Carousel from 'nuka-carousel';
 const athletes = [
   {
     name: "J'Den Cox",
-    styleWrestling: "Men's Freestyle",
+    styleWrestling: "Freestyle",
     weight: "86kg",
     img:
       "https://content.themat.com/images/kidbio-pics/76424401.jpg"
@@ -17,35 +17,59 @@ const athletes = [
     img: "https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/2017-Athlete-Mugs/PatSmith150x250.jpg?mh=250&mw=150&hash=60E752E9A991BD19DE32ABE3BC929C09C5A6741B"
   },
   {
+    name: "Victoria Francis",
+    styleWrestling: "Freestyle",
+    weight: "75kg",
+    img: "https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/2016-Athlete-Mugs/VictoriaFrancis150x250.jpg?mh=250&mw=150&hash=1A3D148FDB017E6FB8FCA773C85A2D5FE90350BB"
+},
+  {
     name: "Nick Gwiazdowski",
-    styleWrestling: "Men's Freestyle",
+    styleWrestling: "Freestyle",
     weight: "125kg",
     img:
       "https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/2017-Athlete-Mugs/NickGwiazdowski150x250.jpg?mh=250&mw=150&hash=057F192D1E61544B1275D243C3F921CBA682FFD4"
   },
   {
-      name: "Victoria Francis",
-      styleWrestling: "Women's Freestyle",
-      weight: "75kg",
-      img: "https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/2016-Athlete-Mugs/VictoriaFrancis150x250.jpg?mh=250&mw=150&hash=1A3D148FDB017E6FB8FCA773C85A2D5FE90350BB"
+    name: "Tamyra Mensah-Stock",
+    styleWrestling: "Freestyle",
+    weight: "68kg",
+    img: "https://content.themat.com/images/kidbio-pics/1801129401.jpg"
+  },
+  {
+      name:"Max Nowry, SSG US Army",
+      styleWrestling: "Greco-Roman",
+      weight: '55kg',
+      img: "https://www.armywcap.com/application/files/6315/3789/4096/ssg-nowry---wrestling_44624084381_o-compressor.jpg"
   },
   {
       name: "Jenna Burkert",
-      styleWrestling: "Women's Freestyle",
+      styleWrestling: "Freestyle",
       weight: '57kg',
       img:"https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/Old-Athlete-Mugs/Burkert150x250.jpg?mh=250&mw=150&hash=CE959EDC2420CBBE5F0BABF9831E72CED816FDF4"
   },
   {
+    name:"John Stefanowicz, SGT USMC",
+    styleWrestling: "Greco-Roman",
+    weight: '82kg',
+    img: "https://i.imgur.com/DW0JzKT.jpg"
+  },
+  {
       name: "Whitney Condor",
-      styleWrestling: "Women's Freestyle",
+      styleWrestling: "Freestyle",
       weight: '53kg',
       img:"https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/2015-Athlete-Mugs/WhitneyConder150x250.jpg?mh=250&mw=150&hash=3CAFEF32A203121D8B1C84C3D20EE63F6EC0D4E7"
   },
   {
       name: "Zain Retherford",
-      styleWrestling: "Men's Freestyle",
+      styleWrestling: "Freestyle",
       weight: '65kg',
       img: "https://www.teamusa.org/-/media/USA_Wrestling/Athlete-Bios/2017-Athlete-Mugs/ZainRetherford150x250.jpg?mh=250&mw=150&hash=C648DBDF59C6ECB357E3FB424679701DD8AA748C"
+  },
+  {
+      name: "Pat Downey III",
+      styleWrestling: "Freestyle",
+      weight: '86kg',
+      img:  "https://bloximages.chicago2.vip.townnews.com/globegazette.com/content/tncms/assets/v3/editorial/5/f6/5f6fb3de-6437-5088-8615-7887d6904c2d/5806a451a1236.image.jpg?resize=300%2C450"
   }
 ];
 
